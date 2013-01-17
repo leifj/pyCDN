73e56a21241fc4e16f172fe0ab151fd47359de42
Leif Johansson <leifj@sunet.se>
2013-01-17 16:10:06 +0100

    simpler

--- src/pycdn/wsgi/hostmeta/__init__.py
+++ src/pycdn/wsgi/hostmeta/__init__.py
@@ -1,14 +1,31 @@
+import hashlib
+
 __author__ = 'leifj'
 
-from pycdn.mt import MerkleTree
 import os
 import json
 from pycdn.wsgi import not_found, dispatcher
 
+def _digest(dir,d=dict()):
+    for path, dirnames, filenames in os.walk(dir):
+        dd = hashlib.sha256()
+        for dir in dirnames:
+            subdir = os.path.join(path,dir)
+            _digest(subdir,d)
+            dd.update(d[subdir])
+        for fn in filenames:
+            subfile = os.path.join(path,fn)
+            md = hashlib.sha256()
+            with open(subfile) as fd:
+                md.update(fd.read())
+            d[subfile] = md.hexdigest()
+            dd.update[d[subfile]]
+        d[path] = dd.hexdigest()
+
 def _mt(environ,start_response):
     dir = os.environ.get("MT_DIR","/var/www")
     start_response("200 OK",[('Content-Type','application/json')])
-    mt = MerkleTree(dir)
+    mt = _digest(dir)
     return [json.dumps(mt)]
 
 urls = [
